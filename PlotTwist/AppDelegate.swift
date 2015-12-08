fb32c1ae9f104f17e28924f29787fd73223b8224
PhiltheHen <philip.d.henson@gmail.com>
2015-12-08 11:57:42 -0800

    added registersubclass for activity in appDelegate

--- PlotTwist/AppDelegate.swift
+++ PlotTwist/AppDelegate.swift
@@ -51,6 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Story.registerSubclass()
         Page.registerSubclass()
         User.registerSubclass()
+        Activity.registerSubclass()
        //does this need to be set to uncomment 
         //return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 
